a23c6e8920af8e07154024ba98870bf0ae3394ad
Ronny Novik <novikdesigndev@gmail.com>
2023-05-21 02:24:09 +0300

    added watch to start:prod

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,6 +14,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     PassportModule,
     JwtModule.registerAsync({
       useFactory: async () => {
+        console.log(process.env.JWT_SECRET_KEY);
         return {
           secret: process.env.JWT_SECRET_KEY,
           signOptions: { expiresIn: process.env.JWT_EXPIRATION_DURATION },
