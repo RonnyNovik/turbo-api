e1cd1002eea01aa55c196576e53d1abf8214db7d
Ronny Novik <novikdesigndev@gmail.com>
2023-05-21 02:30:38 +0300

    added env vars to ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,13 @@ module.exports = {
     {
       script: './src/main.ts',
       watch: '.',
+      env: {
+        DB_USER: 'supertest',
+        DB_PASS: 'sup3rt3st',
+        JWT_SECRET_KEY:
+          'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY4NDYyNDY5NiwiaWF0IjoxNjg0NjI0Njk2fQ.UUeBwLnEXYGnpRDgac09ATfNwLpXQxp3zavDyWti9W4',
+        JWT_EXPIRATION_DURATION: '30d',
+      },
     },
   ],
 };
