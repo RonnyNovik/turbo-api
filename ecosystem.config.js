8f4e195eb35dab18b2b95bffef4e1709eb0b4814
Ronny Novik <novikdesigndev@gmail.com>
2023-05-25 00:54:43 +0300

    fixed ecosystem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   apps: [
     {
+      name: 'api',
       script: './src/main.ts',
-      watch: '.',
+      node_args: '-r esm',
+      watch: ['./src/'],
+      instances: '1',
+      exec_mode: 'fork',
+      watch_options: {
+        persistent: true,
+        ignoreInitial: false,
+      },
     },
   ],
 };
--- startprod.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ./src
-ts-node -r tsconfig-paths/register main.ts
