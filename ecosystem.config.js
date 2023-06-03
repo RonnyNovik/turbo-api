58a56cf3e3ff4b0523e88f471e15eab65f5ed1ae
unknown <rnovik.web@gmail.com>
2023-06-03 04:41:50 +0300

    modified prod script file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,7 @@ module.exports = {
       name: 'api',
       interpreter: './node_modules/.bin/ts-node',
       script: './src/main.ts',
-      node_args: '-r esm',
       watch: ['./src/'],
-      instances: '1',
       exec_mode: 'fork',
       watch_options: {
         persistent: true,
